0ffb1f2a48a2d4d3a866c66c81ce2780c74924c5
SunnyWali <sunnywali777@gmail.com>
2025-05-03 10:31:30 +0530

    made some changes

--- app.js
+++ app.js
@@ -7,7 +7,15 @@ app.get("/",(req,res)=>{
     res.send("Hi i am root");
 })
 
+async function main(){
+    await mongoose.connect("mongodb://127.0.0.1/WanderLust_Project");
+}
 
+main().then(()=>{
+    console.log("successfully connected to the database");
+}).catch((err)=>{
+    console.log(err);
+});
 app.listen(8080,()=>{
     console.log("server is listening to port no 8080");
 })
