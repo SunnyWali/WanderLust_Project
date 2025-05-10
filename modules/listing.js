8053d3392938a6df189746be42bbd37c6d73e0e0
SunnyWali <sunnywali777@gmail.com>
2025-05-10 11:51:31 +0530

    made some changes

--- modules/listing.js
+++ modules/listing.js
@@ -1,9 +1,11 @@
 const mongoose=require("mongoose");
 const listingSchema=new mongoose.Schema({
-    title:String,
+    title:{
+        
+    }
     description:String,
     image:String,
     price:Number,
     country:String,
     location:String
-})
+});
