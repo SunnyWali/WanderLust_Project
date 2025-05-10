1c2f2983d75f725d1310082dbb8b21d6f6b3515e
SunnyWali <sunnywali777@gmail.com>
2025-05-10 11:56:19 +0530

    made some changes

--- modules/listing.js
+++ modules/listing.js
@@ -1,15 +1,15 @@
-const mongoose=require("mongoose");
-const listingSchema=new mongoose.Schema({
-    title:{
-        type:String,
-        required:true
+const mongoose = require("mongoose");
+const listingSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
     },
-    description:{
-        type:String,
-        set:(v) => v=== "" ? "default link" :v,
+    description: {
+        type: String,
+        set: (v) => v === "" ? "https://plus.unsplash.com/premium_photo-1687960116497-0dc41e1808a2?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" : v,
     },
-    image:String,
-    price:Number,
-    country:String,
-    location:String
+    image: String,
+    price: Number,
+    country: String,
+    location: String
 });
